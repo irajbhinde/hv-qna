a88f9998157f83c9f235ef8e38c168c9f82f7bb7
Raj Bhinde <68704186+irajbhinde@users.noreply.github.com>
2022-11-09 20:57:36 +0530

    fix: storing values of question&answers for result

--- src/pages/QuestionPage.jsx
+++ src/pages/QuestionPage.jsx
@@ -7,31 +7,29 @@ export default function QuestionPage() {
   const [selectedOption, setSelectedOption] = useState();
   const { questionState, questionDispatch } = useQuestion();
   const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [currentOptions, setCurrentOptions] = useState([]);
-  const { questionAnswerData } = questionState;
   const [checkedState, setCheckedState] = useState(false);
   const navigate = useNavigate();
-
-  const options = questionData.questions.map((ques) => {
-    let arr = [];
-    return (arr = [...arr, ques.questionoption.map((opt) => opt.optionvalue)]);
+  const [dateValue, setDateValue] = useState("");
+  const [textValue, setTextValue] = useState("");
+  const [checkedInfo, setCheckedInfo] = useState({
+    answersChecked: [],
   });
-  // console.log(options[0]);
-  // console.log(options);
-  // console.log(currentQuestion);
-
   const handleSelect = (names) => {
     setSelectedOption(names);
-    console.log(selectedOption);
-    const resultsPageData = {
-      question: questionData.questions[currentQuestion].question,
-      selectedOption: names,
-    };
-    questionDispatch({ type: "RESULTS_DATA", payload: resultsPageData });
-    // console.log(resultsPageData);
+    if (typeof names === "object") {
+      const checkboxAnswers = {
+        question: questionData.questions[currentQuestion].question,
+        selectedOption: names,
+      };
+      questionDispatch({ type: "CHECKBOX_DATA", payload: checkboxAnswers });
+    } else {
+      const resultsPageData = {
+        question: questionData.questions[currentQuestion].question,
+        selectedOption: names,
+      };
+      questionDispatch({ type: "RESULTS_DATA", payload: resultsPageData });
+    }
   };
-
-  console.log(questionAnswerData);
 
   return (
     <div className="App">
@@ -43,38 +41,69 @@ export default function QuestionPage() {
       </p>
       <div>
         {questionData.questions[currentQuestion].questionoption.map((opt) => {
-          // console.log(opt);
-          // console.log(opt.optionvalue,checkedState);
-
           return (
             <>
-              {/* <p>{opt.optionvalue}</p> */}
-              <p key={opt.optionvalue}>
+              <p key={opt.optionid}>
                 <input
                   defaultChecked={checkedState}
                   name={`question-${questionData.questions[currentQuestion].questionid}`}
                   type={questionData.questions[currentQuestion].questiontype}
                   id={`value-${opt.optionid}`}
-                  value={opt.optionvalue}
+                  value={
+                    questionData.questions[currentQuestion].questiontype ===
+                    "Date"
+                      ? dateValue
+                      : questionData.questions[currentQuestion].questiontype ===
+                        "Textarea"
+                      ? textValue
+                      : opt.optionvalue
+                  }
                   onClick={() => {
                     if (
                       questionData.questions[currentQuestion].questiontype ===
                       "Radio"
                     ) {
-                      // setCheckedState(false);
                       setCheckedState(!opt.selected);
                       setSelectedOption(opt.optionvalue);
-                      // console.log("sel",selectedOption);
-                      console.log(opt.optionvalue, "-", checkedState);
                     }
                   }}
                   onChange={(e) => {
                     if (
-                      questionData.questions[currentQuestion].questiontype !==
-                      "Radio"
+                      questionData.questions[currentQuestion].questiontype ===
+                      "Checkbox"
                     ) {
-                    
+                      const { value, checked } = e.target;
+                      const { answersChecked } = checkedInfo;
+                      let storeStateData;
+                      if (checked) {
+                        storeStateData = {
+                          ...checkedInfo,
+                          answersChecked: [...answersChecked, value],
+                        };
+                      } else {
+                        storeStateData = {
+                          ...checkedInfo,
+                          answersChecked: answersChecked.filter(
+                            (e) => e !== value
+                          ),
+                        };
+                      }
+                      setCheckedInfo(storeStateData);
+                      setSelectedOption(storeStateData);
+                    }
+                    if (
+                      questionData.questions[currentQuestion].questiontype ===
+                      "Date"
+                    ) {
                       setSelectedOption(e.target.value);
+                      setDateValue(e.target.value);
+                    }
+                    if (
+                      questionData.questions[currentQuestion].questiontype ===
+                      "Textarea"
+                    ) {
+                      setSelectedOption(e.target.value);
+                      setTextValue(e.target.value);
                     }
                   }}
                 />
@@ -88,31 +117,64 @@ export default function QuestionPage() {
       </div>
       {currentQuestion === questionData.questions.length - 1 ? (
         <>
-          <button
-            onClick={() => {
-                console.log("look",selectedOption);
-              handleSelect(selectedOption);
-              setSelectedOption("");
-              setCheckedState(false);
-              navigate("/results");
+          <div
+            style={{
+              margin: "2rem",
+              display: "flex",
+              gap: "2rem",
+              justifyContent: "center",
             }}
           >
-            Submit
-          </button>
+            <button
+              onClick={() => {
+                setCurrentQuestion((currentQuestion) => currentQuestion - 1);
+              }}
+            >
+              Prev
+            </button>
+            <button
+              onClick={() => {
+                handleSelect(selectedOption);
+                setSelectedOption("");
+                setCheckedState(false);
+                navigate("/results");
+              }}
+            >
+              Submit
+            </button>
+          </div>
         </>
       ) : (
         <>
-          <button
-            onClick={() => {
-              handleSelect(selectedOption);
-              setCurrentQuestion((currentQuestion) => currentQuestion + 1);
-              setSelectedOption("");
-              setCheckedState(false);
+          <div
+            style={{
+              margin: "2rem",
+              display: "flex",
+              gap: "2rem",
+              justifyContent: "center",
             }}
-            className="btn-nextQuestion"
           >
-            Next Question
-          </button>
+            <button
+              onClick={() => {
+                if (currentQuestion !== 0) {
+                  setCurrentQuestion((currentQuestion) => currentQuestion - 1);
+                }
+              }}
+            >
+              Prev
+            </button>
+            <button
+              onClick={() => {
+                handleSelect(selectedOption);
+                setCurrentQuestion((currentQuestion) => currentQuestion + 1);
+                setSelectedOption("");
+                setCheckedState(false);
+              }}
+              className="btn-nextQuestion"
+            >
+              Next Question
+            </button>
+          </div>
         </>
       )}
     </div>
--- src/pages/ResultsPage.jsx
+++ src/pages/ResultsPage.jsx
@@ -1,8 +1,29 @@
 import { useQuestion } from "../context/question-context";
 
 export default function ResultsPage() {
-  const { questionState, questionDispatch } = useQuestion();
-  const { questionAnswerData } = questionState;
-  console.log(questionAnswerData);
-  return <h1>Results Page</h1>;
+  const { questionState } = useQuestion();
+  const { questionAnswerData, checkboxData } = questionState;
+  return (
+    <div style={{ textAlign: "center" }} className="results-page">
+      <h1>Results Page</h1>
+      {questionAnswerData.map((question) => {
+        return (
+          <>
+            <p>Question : {question.question}</p>
+            <p>Selected Answer : {question.selectedOption}</p>
+          </>
+        );
+      })}
+      {checkboxData.map((ques) => {
+        return (
+          <>
+            <p>Question : {ques.question}</p>
+            {ques.selectedOption.answersChecked.map((ans) => {
+              return <span>Selected Answer : {ans} </span>;
+            })}
+          </>
+        );
+      })}
+    </div>
+  );
 }
